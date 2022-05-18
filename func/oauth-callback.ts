15dc079515fe875bcd427d5a25c374ffb23dfd27
SirH <micahbenac52@gmail.com>
2022-05-18 03:16:21 -0400

    initial start of link page

--- func/oauth-callback.ts
+++ func/oauth-callback.ts
@@ -5,7 +5,7 @@ import {
 } from "discord-api-types/v10";
 
 import { getUserInfo } from "./helpers/user-helpers.js";
-import { createJwt, UserDataPayload } from "./helpers/jwt-helpers.js";
+import { createJwt, type UserDataPayload } from "./helpers/jwt-helpers.js";
 import { makeRequest } from "./helpers/discord-helpers.js";
 
 export const handler: Handler = async (event) => {
@@ -15,40 +15,45 @@ export const handler: Handler = async (event) => {
     };
   }
 
-  if (event.queryStringParameters?.code !== undefined) {
-    const data = {
-      client_id: process.env.DISCORD_CLIENT_ID!,
-      client_secret: process.env.DISCORD_CLIENT_SECRET!,
-      grant_type: "authorization_code",
-      code: event.queryStringParameters.code,
-      redirect_uri: new URL(event.path, process.env.URL).toString(),
-      scope: "identify",
+  if (event.queryStringParameters?.code === undefined)
+    return {
+      statusCode: 400,
     };
 
-    // convert data to x-www-form-urlencoded string
-    const formData = Object.keys(data)
-      .map(
-        (key) =>
-          `${encodeURIComponent(key)}=${encodeURIComponent(
-            data[<keyof typeof data>key]
-          )}`
-      )
-      .join("&");
+  const data = {
+    client_id: process.env.DISCORD_CLIENT_ID!,
+    client_secret: process.env.DISCORD_CLIENT_SECRET!,
+    grant_type: "authorization_code",
+    code: event.queryStringParameters.code,
+    redirect_uri: new URL(event.path, process.env.URL).toString(),
+    scope: "identify",
+  };
 
-    const result = await makeRequest<RESTPostOAuth2AccessTokenResult>({
-      method: "POST",
-      contentType: "application/x-www-form-urlencoded",
-      route: Routes.oauth2TokenExchange(),
-      body: formData,
-    });
+  // convert data to x-www-form-urlencoded string
+  const formData = Object.keys(data)
+    .map(
+      (key) =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(
+          data[<keyof typeof data>key]
+        )}`
+    )
+    .join("&");
 
-    if (!result.ok) {
-      console.log(result.err);
-      throw new Error("Failed to get user access token");
-    }
+  const result = await makeRequest<RESTPostOAuth2AccessTokenResult>({
+    method: "POST",
+    contentType: "application/x-www-form-urlencoded",
+    route: Routes.oauth2TokenExchange(),
+    body: formData,
+  });
 
-    const user = await getUserInfo(result.ok.access_token);
+  if (!result.ok) {
+    console.log(result.err);
+    throw new Error("Failed to get user access token");
+  }
 
+  const user = await getUserInfo(result.ok.access_token);
+
+  if (event.queryStringParameters.origin === "/form") {
     const userPublic = <UserDataPayload>{
       id: user.id,
       avatar: user.avatar,
@@ -71,7 +76,44 @@ export const handler: Handler = async (event) => {
     };
   }
 
+  if (event.queryStringParameters.origin === "/link") {
+    const response = await (<Promise<{ message: string }>>(
+      await fetch(`${process.env.DISCORD_LINK_API_URL}/userdata`, {
+        method: "POST",
+        headers: {
+          Authorization: process.env.DISCORD_LINK_API_KEY!,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: user.id,
+          player_id: event.queryStringParameters.playerId,
+          player_token: event.queryStringParameters.playerToken,
+        }),
+      })
+    )
+      .json()
+      .catch((e) => ({ message: e.message })));
+
+    if (!response.message)
+      return {
+        statusCode: 400,
+        headers: {
+          Location: "/error?msg=Failed to link account",
+        },
+      };
+
+    return {
+      statusCode: 303,
+      headers: {
+        Location: "/success?msg=" + encodeURIComponent(response.message),
+      },
+    };
+  }
+
   return {
     statusCode: 400,
+    headers: {
+      Location: "/error",
+    },
   };
 };
--- func/oauth.ts
+++ func/oauth.ts
@@ -2,18 +2,31 @@ import type { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event) => {
   const redirectUri = new URL(
-    "/.netlify/functions/oauth-callback",
+    `/.netlify/functions/oauth-callback?origin=${event.path}`,
     process.env.URL
   );
+
+  const scope = event.path == "/form" ? "identify%20email" : "identify";
 
   let url = `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(
     <string>process.env.DISCORD_CLIENT_ID
   )}&redirect_uri=${encodeURIComponent(
     redirectUri.toString()
-  )}&response_type=code&scope=identify%20email&prompt=none`;
+  )}&response_type=code&scope=${scope}&prompt=none`;
 
   if (event.queryStringParameters?.state !== undefined) {
     url += `&state=${encodeURIComponent(event.queryStringParameters.state)}`;
+  }
+
+  if (
+    event.queryStringParameters?.playerId !== undefined &&
+    event.queryStringParameters.playerToken !== undefined
+  ) {
+    url += `&playerId=${encodeURIComponent(
+      event.queryStringParameters.playerId
+    )}&playerToken=${encodeURIComponent(
+      event.queryStringParameters.playerToken
+    )}`;
   }
 
   return {
